683fff84366d2d618b97a76b3d2f891e8d28ec78
ZhangLeTian <zlt1379@163.com>
2023-10-21 13:11:48 +0800

    change cifar10 GAN

--- cVAE_GAN_cifar10.py
+++ cVAE_GAN_cifar10.py
@@ -83,11 +83,13 @@ class Decoder_Conv_CBN(nn.Module):
         self.conv=nn.Conv2d(num_features,num_features,3,1,'same')
         self.cbn=ConditionalBatchNorm2d(self.num_features,10)
         self.linear=nn.Linear(embed_size+chunksize,10)
+        self.relu=nn.ReLU()
     
     def forward(self,x,y):
         x=self.conv(x)
-        x=self.linear(y)
-        x=nn.ReLU(self.cbn(x,y))
+        y=self.linear(y)
+        x=self.cbn(x,y)
+        x=self.relu(x)
         return x
 
 
@@ -105,12 +107,13 @@ class DecoderBlock(nn.Module):
         self.conv=nn.Conv2d(num_features,num_features,1,1,'same')
         self.linear=nn.Linear(embed_size+chunksize,10)
         self.cbn=ConditionalBatchNorm2d(self.num_features,10)
-        self.upsample=nn.Upsample(size=(self.gen_size*2, self.gen_size*2), mode='bilinear', align_corners=True)
-        
+        self.upsample1=nn.Upsample(size=(self.gen_size*2, self.gen_size*2), mode='bilinear', align_corners=True)
+        self.upsample2=nn.Upsample(size=(self.gen_size*2, self.gen_size*2), mode='bilinear', align_corners=True)
+        self.relu=nn.ReLU()
 
         #生成len(ys)个卷积CBN块
         self.conv_cbns=nn.ModuleList()
-        for i in range(self.ys_length):  
+        for i in range(self.ys_length-1):  
             conv_cbn=Decoder_Conv_CBN(gen_size,
                                       num_features,
                                       embed_size,
@@ -123,16 +126,18 @@ class DecoderBlock(nn.Module):
         z1=self.conv(z1)
 
         y1=self.linear(ys[0])
-        z2=nn.ReLU(self.cbn1(z,y1))
-        z2=self.upsample(z2)
+        z2=self.cbn(z,y1)
+        z2=self.relu(z2)
+        z2=self.upsample2(z2)
         for i,conv_cbn in enumerate(self.conv_cbns):
             z2=conv_cbn(z2,ys[i+1])
         h=z1+z2
-        return h
-        
+        return h            
 
 class Decoder_cifar10(nn.Module):
-    def __init__(self,z_dimension=80,chunksize=20,num_features=384,gen_size=4,embed_size=128):
+    def __init__(self,z_dimension=80,chunksize=20,
+                 num_features=384,gen_size=4,
+                 embed_size=128,device="cpu"):
         super(Decoder_cifar10,self).__init__()
 
         self.z_dimension=z_dimension 
@@ -140,40 +145,97 @@ class Decoder_cifar10(nn.Module):
         self.num_features=num_features  
         self.gen_size=gen_size
         self.embed_size=embed_size
-        self.linear=nn.Linear(chunksize,384*gen_size*gen_size)
-        self.embed=nn.Embedding(10,embed_size)
+        self.device=device
+
+        self.linear=nn.Linear(chunksize,384*gen_size*gen_size).to(self.device)
+        self.embed=nn.Embedding(10,embed_size).to(self.device)
+        
         #定义生成块
         self.gen_blocks=nn.ModuleList()
         for i in range((z_dimension//chunksize)-1):  
-            gen_block = DecoderBlock(self.gen_size * 2**(i+1),
+            gen_block = DecoderBlock(self.gen_size *2**(i),
                                      self.num_features,
                                      self.embed_size,
                                      self.chunksize,
                                      (z_dimension//chunksize)-1
-                                     )  # 使用不同的 gen_size
+                                     ).to(self.device)  # 使用不同的 gen_size
             self.gen_blocks.append(gen_block)
-        self.BnReLu=nn.ReLU(nn.BatchNorm2d(384))
-        self.conv=nn.Conv2d(384,3,kernel_size=3,stride=1,padding='same')
-        self.tanh=nn.Tanh()
+        self.BnReLu=nn.Sequential(
+                    nn.BatchNorm2d(384),
+                    nn.ReLU()).to(self.device)
+        self.conv=nn.Conv2d(384,3,kernel_size=3,stride=1,padding='same').to(self.device)
+        self.tanh=nn.Tanh().to(device)
     def forward(self,z,y):
         #将z切片
-        zs = torch.split(z, self.chunksize, dim=0)
-        z=zs[0]
-        ys=zs[1:]
+        zs = torch.split(z, self.chunksize, dim=1)
+        z=zs[0].to(self.device)
+        zs=zs[1:]
         y=self.embed(y)
+        y=y.to(self.device)
         #拼接
-        for i in range(len(ys)):
-            zs[i] = torch.cat((y, ys[i]), dim=0)
+        ys=[]
+        for i in range(len(zs)):
+            ys.append(torch.cat((y, zs[i].to(self.device)), dim=1))
         z=self.linear(z)
         z=z.view(-1, 384, 4, 4)
-        for i,genblock in enumerate(self.genblocks):
+        for i,genblock in enumerate(self.gen_blocks):
             z=genblock(z,ys)
         z=self.BnReLu(z)
         z=self.conv(z)
         z=self.tanh(z)
         return z
-            
 
+
+# class Decoder_cifar10(nn.Module):
+#     def __init__(self,z_dimension=80,chunksize=20,
+#                  num_features=384,gen_size=4,
+#                  embed_size=128):
+#         super(Decoder_cifar10,self).__init__()
+
+#         self.z_dimension=z_dimension 
+#         self.chunksize=chunksize  
+#         self.num_features=num_features  
+#         self.gen_size=gen_size
+#         self.embed_size=embed_size
+        
+
+#         self.linear=nn.Linear(chunksize,384*gen_size*gen_size)
+#         self.embed=nn.Embedding(10,embed_size)
+        
+#         #定义生成块
+#         self.gen_blocks=nn.ModuleList()
+#         for i in range((z_dimension//chunksize)-1):  
+#             gen_block = DecoderBlock(self.gen_size *2**(i),
+#                                      self.num_features,
+#                                      self.embed_size,
+#                                      self.chunksize,
+#                                      (z_dimension//chunksize)-1
+#                                      )  # 使用不同的 gen_size
+#             self.gen_blocks.append(gen_block)
+#         self.BnReLu=nn.Sequential(
+#                     nn.BatchNorm2d(384),
+#                     nn.ReLU())
+#         self.conv=nn.Conv2d(384,3,kernel_size=3,stride=1,padding='same')
+#         self.tanh=nn.Tanh()
+#     def forward(self,z,y):
+#         #将z切片
+#         zs = torch.split(z, self.chunksize, dim=1)
+#         z=zs[0]
+#         zs=zs[1:]
+#         y=self.embed(y)
+#         y=y
+#         #拼接
+#         ys=[]
+#         for i in range(len(zs)):
+#             ys.append(torch.cat((y, zs[i]), dim=1))
+#         z=self.linear(z)
+#         z=z.view(-1, 384, 4, 4)
+#         for i,genblock in enumerate(self.gen_blocks):
+#             z=genblock(z,ys)
+#         z=self.BnReLu(z)
+#         z=self.conv(z)
+#         z=self.tanh(z)
+#         return z
 
         
 
